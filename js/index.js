5b1d00a6107acc71de68483a64203f9a9a0874dc
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-18 00:45:58 +0100

    Prepare asking for geolocation permission

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ button.addEventListener(
   "click",
   () => (window.location.href = "./app/login.html")
 );
+
+// TODO add error handling
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude + ", " + position.coords.longitude);
+  });
+}
