03bed505146b4d66b670e6256e60de6246f51d02
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-17 23:59:43 +0100

    Add Firebase configuration

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-QzwHQoJyMn86RRlXCPUYTFQcsZJ3bsk",
+  authDomain: "leamap-d7b0e.firebaseapp.com",
+  projectId: "leamap-d7b0e",
+  storageBucket: "leamap-d7b0e.appspot.com",
+  messagingSenderId: "152787458052",
+  appId: "1:152787458052:web:3556c386d3ec22507afd51",
+};
+
+const app = initializeApp(firebaseConfig);
