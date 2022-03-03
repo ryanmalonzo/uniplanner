4c69defe1de127f494fdfa63cea9721e986ec1c0
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-03 15:56:03 +0100

    dark => light

--- js/leaflet.js
+++ js/leaflet.js
@@ -14,7 +14,7 @@ const map = L.map("map", {
 	13 // zoom
 );
 L.tileLayer(
-	"https://api.mapbox.com/styles/v1/yusa-ai/ckzsvx2du001e14ngnkdelz1r/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieXVzYS1haSIsImEiOiJja3pmeDhyaWQyeGphMnZuOWU3cjFycWoxIn0.y9pV4e0r27XhX--vc6HVxA"
+	"https://api.mapbox.com/styles/v1/yusa-ai/cl0b3ef5u001314o17n961mja/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieXVzYS1haSIsImEiOiJja3pmeDhyaWQyeGphMnZuOWU3cjFycWoxIn0.y9pV4e0r27XhX--vc6HVxA"
 ).addTo(map);
 
 // map.zoomControl.setPosition("bottomright");
